42bd42fda7f9334b6b3e9f9e2209badff196d82d
tombena <tbenacin@gmail.com>
2017-11-12 14:12:10 +0100

    Append "_" to index field if already present

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import json, os, io, fcntl
 from pprint import pprint
 from pathlib import Path
 
+# if there are nested lists then there could be collision
+# with "_index", therefore if "_index" already exists
+# prefix with another "_" etc...
+def new_index(j_ids):
+
+	curr_index = "_index"
+
+	while (curr_index in j_ids):
+		curr_index = "_" + curr_index
+
+	return curr_index
+
 # add JSON dictionary to a new file, edit file if necessary
 # filename: name of the edited file
 # dic: JSON dictionary that should be added
@@ -50,59 +62,62 @@ def write_to_file(filename, d):
 # depth + 1: number of times this function was called
 # j_ids: dict of JSON ids
 def flatten(obj, parent_name, key, depth, j_ids):
-	# print("depth = " + str(depth))
-	# print("key = " + key)
-	# print("parent_name = " + parent_name)
 
 	# depth has to be smaller than 5
 	if (depth > 5):
 		return
-	# elif (depth != 0):
-	# 	key = key[:-1]
 
 	# create dictionary
 	d = {}
 
-
-
-	# print(obj)
-	# print(type(obj))
-
-	# if type(obj) == dict and len(obj) == 1:
-	# 	elem = obj
+	# set JSON id fields (there can be multiple of them)
+	# j_ids shouldn't be modified
+	d = {**d, **j_ids}
 
 	elem = obj
 	if len(obj) == 1:
 		try:
 			elem = obj[key][0]
 		except KeyError:
-			print(" ")
+			elem = obj
 
 	# for each field in dictionary
 	for key, value in elem.items():
 		if type(value) == dict:
 			# print(value)
 			# print("len = " + str(len(value)))
-			flatten(value, parent_name + "_" + key, key, depth + 1, j_ids)
+			flatten(value, parent_name + "_" + key, key, depth + 1, j_ids.copy())
 		elif type(value) == list:
-			# need to iterate through the list
-			for i in range(len(value)):
-				j_ids["_index"] = i
-				# key is plural therefore the last 's' should be removed
-				# (considering plural of name doesn't become "-es" e.g. baby -> babies)
+			# print(value)
 
-				flatten(value[i], parent_name + "_" + key[:-1], key, depth + 1, j_ids)
+			if len(value) == 0:
+				d[key] = value
+			elif type(value[0]) != dict:
+				d[key] = value
+			else:
+				# need to iterate through the list
+				for i in range(len(value)):
+
+					if len(value) > 1:
+						# need a new field (explicit copy)
+						t_ids = j_ids.copy()
+
+						curr_index = new_index(j_ids)
+
+						# create new "_index" field if necessary (add "_")
+						t_ids[curr_index] = i
+
+						# key is plural therefore the last 's' should be removed
+						# (considering plural of name doesn't become "-es" e.g. baby -> babies)
+
+						flatten(value[i], parent_name + "_" + key[:-1], key, depth + 1, t_ids.copy())
+					else:
+						flatten(value[i], parent_name + "_" + key[:-1], key, depth + 1, j_ids.copy())
 		else:
 			d[key] = value
 
-	# print(parent_name + " of depth " + str(depth) + ":")
-	# print(d)
-
-	if len(d) == 0:
+	if len(d) == len(j_ids):
 		return
-
-	# set JSON id fields (there can be multiple of them)
-	d = {**d, **j_ids}
 
 	write_to_file('output/' + parent_name + '.json', d)
 
@@ -110,7 +125,7 @@ def flatten(obj, parent_name, key, depth, j_ids):
 
 # input files are in input/ folder
 for file in os.listdir("input/"):
-	if file.endswith("donuts.json"):
+	if file.endswith(".json"):
 		# for every file in input/ folder
 		input = open("input/" + file, 'r')
 
@@ -119,10 +134,8 @@ for file in os.listdir("input/"):
 
 		# remove ".json" (5 char long) from filename
 		parent_name = file[:-5];
-		# print(parent_name)
 
 		j_ids = {"id": data[parent_name][0]["id"]}
-		# print(j_ids)
 
 		flatten(data, parent_name[:-1], parent_name, 0, j_ids)
 
